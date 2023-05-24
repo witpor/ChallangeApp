e04d8027727aa2b43c908e57e514ccb83ac9ff32
Witold Porosiło <vitekp@hotmail.com>
2023-05-24 22:37:44 +0200

    Lekcja 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Witold!");
+﻿var sex = "F";
+var name = "Ewa";
+var age = 33;
+
+if (sex == "F" && name == "Ewa" && age == 33)
+{
+    Console.WriteLine(name + ", lat " +  age);
+}
+
+else if (sex != "F" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (sex == "F" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30");
+}
+else
+    Console.WriteLine("Dane są spoza zakresu");
